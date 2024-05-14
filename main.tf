3358914148a6e2bca3b0dfa5f0aadd5d46db9757
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-05-14 13:39:06 -0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,8 @@ provider "aws" {
 resource "aws_sns_topic" "sns_topic" {
   name = var.sns_topic_name
 }
+
+output "sns_topic_arn" {
+  description = "The ARN of the created SNS topic."
+  value       = aws_sns_topic.sns_topic.arn
+}
