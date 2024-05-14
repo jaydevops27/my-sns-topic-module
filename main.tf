e3374a11e74913ba6035d6dac78cad761ca1a168
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-05-14 14:23:02 -0400

    updated

--- main.tf
+++ main.tf
@@ -5,22 +5,9 @@ provider "aws" {
 resource "aws_sns_topic" "sns_topic" {
   name              = var.sns_topic_name
   display_name      = var.sns_topic_name
-  policy            = data.aws_iam_policy_document.sns_topic.json
-  delivery_policy   = var.sns_delivery_policy
-  application_succe = var.application_succe
-  application_fail  = var.application_fail
-  endpoint          = var.sns_topic_endpoint
-  protocol          = var.sns_topic_protocol
 }
 
-data "aws_iam_policy_document" "sns_topic" {
-  statement {
-    actions   = ["sns:Publish"]
-    resources = [aws_sns_topic.sns_topic.arn]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
+output "sns_topic_arn" {
+  description = "The ARN of the created SNS topic."
+  value       = aws_sns_topic.sns_topic.arn
 }
--- variables.tf
+++ variables.tf
@@ -7,31 +7,3 @@ variable "sns_topic_name" {
   description = "The name for the SNS topic."
   type        = string
 }
-
-variable "sns_topic_endpoint" {
-  description = "The endpoint that receives notifications from this SNS topic."
-  type        = string
-}
-
-variable "sns_topic_protocol" {
-  description = "The protocol for the endpoint."
-  type        = string
-}
-
-variable "sns_delivery_policy" {
-  description = "The delivery policy for the SNS topic."
-  type        = string
-  default     = "arn:aws:sns:us-east-1:123456789012:default-delivery-policy"
-}
-
-variable "application_succe" {
-  description = "The success application to receive messages."
-  type        = string
-  default     = "arn:aws:sns:us-east-1:123456789012:default-success-application"
-}
-
-variable "application_fail" {
-  description = "The failure application to receive messages."
-  type        = string
-  default     = "arn:aws:sns:us-east-1:123456789012:default-failure-application"
-}
