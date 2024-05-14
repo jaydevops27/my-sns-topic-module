02b389858789c56380ec504e310bb53c4eac4a72
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-05-14 14:27:57 -0400

    updated

--- main.tf
+++ main.tf
@@ -3,11 +3,28 @@ provider "aws" {
 }
 
 resource "aws_sns_topic" "sns_topic" {
-  name              = var.sns_topic_name
-  display_name      = var.sns_topic_name
+  name         = var.sns_topic_name
+  display_name = var.sns_topic_name
+}
+
+data "aws_iam_policy_document" "sns_topic" {
+  statement {
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.sns_topic.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
 
 output "sns_topic_arn" {
   description = "The ARN of the created SNS topic."
   value       = aws_sns_topic.sns_topic.arn
+}
+
+output "sns_topic_policy_json" {
+  description = "The JSON representation of the IAM policy document."
+  value       = data.aws_iam_policy_document.sns_topic.json
 }
