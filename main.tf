b10401befa854ed792d74fa6157fe9640bd29bb2
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-05-15 14:29:24 -0400

    updated

--- main.tf
+++ main.tf
@@ -7,30 +7,7 @@ resource "aws_sns_topic" "sns_topic" {
   display_name = var.sns_topic_name
 }
 
-data "aws_iam_policy_document" "sns_topic" {
-  statement {
-    actions   = ["sns:Publish"]
-    resources = [aws_sns_topic.sns_topic.arn]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_sns_topic_subscription" "sns_subscription" {
-  topic_arn = aws_sns_topic.sns_topic.arn
-  protocol  = var.sns_subscription_protocol
-  endpoint  = var.sns_subscription_endpoint
-}
-
 output "sns_topic_arn" {
   description = "The ARN of the created SNS topic."
   value       = aws_sns_topic.sns_topic.arn
-}
-
-output "sns_topic_policy_json" {
-  description = "The JSON representation of the IAM policy document."
-  value       = data.aws_iam_policy_document.sns_topic.json
 }
--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "sns_topic_name" {
   description = "The name for the SNS topic."
   type        = string
 }
-
-variable "sns_subscription_endpoint" {
-  description = "The endpoint that receives notifications from this SNS topic."
-  type        = string
-}
-
-variable "sns_subscription_protocol" {
-  description = "The protocol for the endpoint."
-  type        = string
-}
